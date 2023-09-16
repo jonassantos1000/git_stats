c490b79b5d8fc4706d7af55c8ba873b2f0ae86d9
jonassantos1000 <jonasvale3@hotmail.com>
2023-09-16 20:14:36 -0300

    ajustado logica para retornar resultados completos

--- service/ReportService.py
+++ service/ReportService.py
@@ -17,7 +17,7 @@ class ReportService:
         self.token = token
         self.id_usuario = id_usuario
 
-        commit_ordenados = sorted(self.__buscar_commits(), key=lambda x: self.__formatar_data(x["created_at"]))
+        commit_ordenados = sorted(self.__buscar_commits(), key=lambda x: self.__formatar_data(x.get("created_at", "")))
 
         lista_commit_detalhes = []
 
@@ -25,28 +25,38 @@ class ReportService:
             if not commit.get("push_data").get("commit_to"):
                 continue
 
-            lista_commit_detalhes.append(self.__buscar_detalhes_commit(commit.get("project_id"), commit.get("push_data").get("commit_to")))
+            lista_commit_detalhes.append(
+                self.__buscar_detalhes_commit(commit.get("project_id"), commit.get("push_data").get("commit_to"))
+            )
 
         return self.__agrupar_commits(lista_commit_detalhes)
-
 
     def __buscar_commits(self):
 
         url = f"{self.url_base}/users/{self.id_usuario}/events"
+        list_commits = []
 
-        params = {
-            "action": "pushed",
-            "private_token": self.token,
-            "after": (self.data_atual - timedelta(days=3)).strftime("%Y-%m-%d"),
-            "before": (self.data_atual + timedelta(days=2)).strftime("%Y-%m-%d"),
-            "per_page": "100",
-            "page": "1"
-        }
+        pagina = 0
+        while True:
+            params = {
+                "action": "pushed",
+                "private_token": self.token,
+                "after": (self.data_atual - timedelta(days=8)).strftime("%Y-%m-%d"),
+                "before": (self.data_atual + timedelta(days=1)).strftime("%Y-%m-%d"),
+                "per_page": "100",
+                "page": {pagina}
+            }
 
-        response = requests.get(url, params)
+            response = requests.get(url, params).json()
 
-        return response.json()
+            if not response:
+                break
 
+            list_commits.extend(response)
+
+            pagina += 1
+
+        return list_commits
 
     def __buscar_detalhes_commit(self, id_project, hash_commit):
         url = f"{self.url_base}/projects/{id_project}/repository/commits/{hash_commit}"
@@ -58,7 +68,6 @@ class ReportService:
 
         response = requests.get(url, params)
         return response.json()
-
 
     def __formatar_data(self, data_str):
         data = datetime.strptime(data_str, "%Y-%m-%dT%H:%M:%S.%f%z")
